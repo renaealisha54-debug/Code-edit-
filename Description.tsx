96f97d2d47d11e54a6fb30ee495ea92446f67a03
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-10 19:01:09 -0400

    Create Description.tsx

---
+++ Description.tsx
@@ -0,0 +1,37 @@
+Im making a app i want it to use the same system firebase (as far as edit and revise )   
+
+What's going to be different is im going to add a notepad overlay ( movable minimizable and size adjustable ). 
+
+Its going to read edit correct build and run all code languages types ( python,react native, c, c+  , Java, gradle, cloude ,firebase code , and grok code ) 
+ 
+Im going to add the option “export “ export will have the options ( export code as one whole document at all times ) and other option is export apk file 
+ 
+When export is exported make sure its not a zip i want it to be as a whole file like one page document whatever ) 
+ 
+Adding a preview code progress ( like prototype preview ) would be cool but if its too difficult its okay ) 
+
+Add the option to view terminals , or make a blank field always on the main page to upload code to. (Partial , whole , whatever)   then also in the blank field add the options (to actions ) 
+-Edit code 
+-Correct code 
+-Build code 
+-run code 
+-export code(doc file ) 
+-convert code language  
+-export apk 
+-export template ( separate code components) 
+-share (sms or email ) whole code one page.  
+
+
+It has to have the ability to sort throught and differentiate different code languages types. (All the code types i said earlier ) 
+ 
+The overlay (once enabled) is just a blank field notepad that draws over other apps to prevent having to exit from pages . 
+
+The app opens up as a blank field int he middle of the page with the options insaid earlier.  All the way at the top to the left options to see recent projects option to run terminal upload files option (to upload pre downloaded code files or zips from device) had to have the ability to read revise and correct that field also.  And also the option to use a app building template( thats has all the building fields separated and explained so code can be entered and built one section at a time. Now option at the top of the page in the middle says export whole code(doc file) now option to the far right says export apk ( has to edit revise run and correct all codes types and have the ability to export and create a apk file correctly with all the necessary permissions and stuff included.  
+
+
+Also make sure the api keys and stuff are all legit and working correctly.  
+
+Add all nessasary extentions and permissions 
+
+
+
